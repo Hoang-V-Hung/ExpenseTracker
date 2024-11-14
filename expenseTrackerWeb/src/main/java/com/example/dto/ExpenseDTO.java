8b8ad3418c19bb6e1bdd319ac12ebe45e9d6ceac
Hoang-V-Hung <HungHv.b23cc076@stu.ptit.edu.vn>
2024-11-15 00:00:54 +0700

    update

--- expenseTrackerWeb/src/main/java/com/example/dto/ExpenseDTO.java
+++
@@ -1,8 +0,0 @@
-public class ExpenseDTO {
-    private BigDecimal amount;
-    private LocalDate date;
-    private String category;
-    private String description;
-    
-    // getters and setters
-} 
